ba11577bb16dae9b7bd35fb71eefcd608d6dfe54
Lexacle Technologies Ltd <lexacletechnologies@gmail.com>
2024-07-01 13:28:42 +0300

    Updated Meta Data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,49 @@ import "./global.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const meta: any = {
+  title: "Mwanzo Place | Tigoni Limuru Road",
+  description: "3228 Tigoni - Limuru Road - hello@mwanzo.co.ke",
+  type: "website",
+  card: "summary_large_image",
+  site: "@mwanzo",
+  creator: "@mwanzo",
+  images: "/assets/img/meta/icon.png",
+  url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+};
 export const metadata: Metadata = {
-  title: "Mwanzo Place - Tigoni Limuru Road",
-  description: "Mwanzo Place Tigoni - Limuru Road",
+  metadataBase: new URL(meta.url),
+  alternates: {
+    canonical: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+  },
+  title: {
+    template: "%s - Mwanzo Place",
+    default: "Mwanzo Place Tigoni - Limuru Road",
+  },
+  description: meta.description,
+
+  openGraph: {
+    title: {
+      template: "%s - Mwanzo Place",
+      default: "Mwanzo Place Tigoni - Limuru Road",
+    },
+    description: meta.description,
+    type: meta.type,
+    images: meta.images,
+    url: `${meta.url}`,
+    locale: "en_US",
+  },
+  twitter: {
+    title: {
+      template: "%s - Mwanzo Place",
+      default: "Mwanzo Place Tigoni - Limuru Road",
+    },
+    description: meta.description,
+    card: meta.card,
+    site: meta.site,
+    creator: meta.creator,
+    images: meta.images,
+  },
 };
 
 export default function RootLayout({
