ad2123beaf6f43d6cc8db4c660241c8d7d22cd9c
Lexacle Technologies Ltd <lexacletechnologies@gmail.com>
2024-07-01 21:41:53 +0300

    Reduced Phone Number Size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       />
       <div className="absolute w-full h-full bg-gradient-to-b from-slate-950/[.5] to-slate-900/[.1] flex flex-col items-center justify-center gap-8">
         <div className="text-center text-slate-50 flex flex-col items-center">
-          <p className="text-slate-50 text-xl md:text-4xl font-light italic">
+          <p className="text-slate-50 text-lg md:text-2xl font-light italic">
             Discover your dream home with us
           </p>
         </div>
@@ -35,14 +35,14 @@ export default function Home() {
             href="mailto:hello@mwanzo.co.ke"
             className="transition-all duration-300 text-slate-50 font-normal text-sm flex flex-row items-center gap-2 border border-slate-50 px-4 py-2 rounded hover:bg-slate-50 hover:text-slate-800"
           >
-            <IoMail size={28} />
+            <IoMail size={20} />
             hello@mwanzo.co.ke
           </Link>
           <Link
             href="tel:+254752993333"
             className="transition-all duration-300 text-slate-50 font-normal text-sm flex flex-row items-center gap-2 border border-slate-50 px-4 py-2 rounded hover:bg-slate-50 hover:text-slate-800"
           >
-            <IoCall size={28} />
+            <IoCall size={20} />
             0752 993 333
           </Link>
         </div>
